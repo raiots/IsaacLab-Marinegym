35f0bcceeadb6ee48a07b9a285d38dbdfd685022
shuguang chu <12125072@zju.edu.cn>
2025-02-05 13:56:37 +0800

    update

---
+++ docker/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+pip install -e /workspace/MarineGym
+exec "$@"
--- docker/install_maringym.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 安装 OmniDrones
-/workspace/isaaclab/_isaac_sim/python.sh -m pip install -e /workspace/OmniDrones
-
-# 执行原有的 entrypoint 命令
-exec "$@"
